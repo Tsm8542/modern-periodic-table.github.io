2c5389ff6ec3848d38afefa9bea6eb3370fad563
Tsm8542 <shaikhabid932@gmail.com>
2022-06-21 12:44:26 +0530

    .

---
+++ Modern Periodic Table/Elements html/Period 5/script.js
@@ -0,0 +1,129 @@
+function gp(){
+    var x = document.getElementById("gp");
+    var y = document.getElementById("x1");
+    if(x.style.display==="none"){
+        x.style.display="";
+        y.innerHTML="&downarrow;"
+    }
+    else{
+        x.style.display="none";
+        y.innerHTML="&uparrow;"
+    }
+}
+
+function loc(){
+    var x = document.getElementById("loc");
+    var y = document.getElementById("x2");
+    if(x.style.display==="none"){
+        x.style.display="";
+        y.innerHTML="&downarrow;"
+    }
+    else{
+        x.style.display="none";
+        y.innerHTML="&uparrow;"
+    }
+}
+
+function his(){
+    var x = document.getElementById("his");
+    var y = document.getElementById("x3");
+    if(x.style.display==="none"){
+        x.style.display="";
+        y.innerHTML="&downarrow;"
+    }
+    else{
+        x.style.display="none";
+        y.innerHTML="&uparrow;"
+    }
+}
+
+function pp(){
+    var x = document.getElementById("pp");
+    var y = document.getElementById("x4");
+    if(x.style.display==="none"){
+        x.style.display="";
+        y.innerHTML="&downarrow;"
+    }
+    else{
+        x.style.display="none";
+        y.innerHTML="&uparrow;"
+    }
+}
+
+function ad(){
+    var x = document.getElementById("ad");
+    var y = document.getElementById("x5");
+    if(x.style.display==="none"){
+        x.style.display="";
+        y.innerHTML="&downarrow;"
+    }
+    else{
+        x.style.display="none";
+        y.innerHTML="&uparrow;"
+    }
+}
+
+function radius(){
+    var x = document.getElementById("radius");
+    var y = document.getElementById("x6");
+    if(x.style.display==="none"){
+        x.style.display="";
+        y.innerHTML="&downarrow;"
+    }
+    else{
+        x.style.display="none";
+        y.innerHTML="&uparrow;"
+    }
+}
+
+function ec(){
+    var x = document.getElementById("ec");
+    var y = document.getElementById("x7");
+    if(x.style.display==="none"){
+        x.style.display="";
+        y.innerHTML="&downarrow;"
+    }
+    else{
+        x.style.display="none";
+        y.innerHTML="&uparrow;"
+    }
+}
+
+function stable(){
+    var x = document.getElementById("stable");
+    var y = document.getElementById("x8");
+    if(x.style.display==="none"){
+        x.style.display="";
+        y.innerHTML="&downarrow;"
+    }
+    else{
+        x.style.display="none";
+        y.innerHTML="&uparrow;"
+    }
+}
+
+function enthalpy(){
+    var x = document.getElementById("enthalpy");
+    var y = document.getElementById("x9");
+    if(x.style.display==="none"){
+        x.style.display="";
+        y.innerHTML="&downarrow;"
+    }
+    else{
+        x.style.display="none";
+        y.innerHTML="&uparrow;"
+    }
+}
+
+function radnhf(){
+    var x = document.getElementById("radnhf");
+    var y = document.getElementById("x10");
+    if(x.style.display==="none"){
+        x.style.display="";
+        y.innerHTML="&downarrow;"
+    }
+    else{
+        x.style.display="none";
+        y.innerHTML="&uparrow;"
+    }
+}
